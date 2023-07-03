fc624059aab7d91dc77127c0cee3ece0789a1144
almaz <almazgimaev@gmail.com>
2023-07-03 11:13:42 +0300

    set preview image id

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ HOMEPAGE_URL: str = (
 )
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1434437
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/pothole-dataset-v8-for-detection"
