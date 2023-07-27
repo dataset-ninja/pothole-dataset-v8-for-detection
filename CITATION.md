5eac341b40a0ab2749d3c489473a43ca0d158e05
Ars Vladimirov <grokhael@gmail.com>
2023-07-27 17:58:23 +0600

    upd custom dta

--- CITATION.md
+++ CITATION.md
@@ -1,11 +1,11 @@
 If you make use of the Pothole dataset v8 for detection data, please cite the following reference:
 
 ``` bibtex 
-@misc{Pothole dataset v8 for detection,
-	author="Sovit Rath",
-	title="Pothole dataset v8 for detection",
+@dataset{Pothole dataset v8 for detection,
+	author={Sovit Rath and Dmitrii Nosov},
+	title={Pothole dataset v8 for detection},
 	year={2023},
-	url="https://www.kaggle.com/datasets/denisg04/pothle-detect?select=separate_labels_given_frames.py"
+	url={https://www.kaggle.com/datasets/denisg04/pothle-detect?select=separate_labels_given_frames.py}
 }
 ```
 
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,11 +20,16 @@ PROJECT_NAME_FULL: str = "Pothole dataset v8 for detection"
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC0_1_0()
-INDUSTRIES: List[Industry] = [Industry.Industrial()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Automotive(is_used=False)]
+CATEGORY: Category = Category.SelfDriving()
+
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = 2023
+RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = 2023
+
 HOMEPAGE_URL: str = (
     "https://www.kaggle.com/datasets/denisg04/pothle-detect?select=separate_labels_given_frames.py"
 )
@@ -33,7 +46,7 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/pothole-dataset-v8-for-detec
 ##################################
 DOWNLOAD_ORIGINAL_URL: Optional[
     Union[str, dict]
-] = "https://www.dropbox.com/s/qvglw8pqo16769f/pothole_dataset_v8.zip?dl=1"
+] = "hhttps://www.kaggle.com/datasets/denisg04/pothle-detect/download?datasetVersionNumber=1"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -41,8 +54,12 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = None
+AUTHORS: Optional[List[str]] = ["Sovit Rath", "Dmitrii Nosov"]
+
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -57,10 +74,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -72,13 +94,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
